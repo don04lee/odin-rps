b434236ccd8d0271ac2deed9cf6e112a1760b098
Dong Woo Lee <don04lee@gmail.com>
2023-07-19 17:23:44 -0700

    Complete rock paper scissors with decent ui
    Implemented reset button as well as making the entire
    rock paper scissors game just work. Logic of game itself
    has been modified using global vars of player and computer's score.

--- index.js
+++ index.js
@@ -1,99 +1,116 @@
-const buttons = document.querySelectorAll('button');
-buttons.forEach(button => {
-  button.addEventListener('click', function(e) {
-    const playerOption = this.id;
-    console.log(playRound(playerOption, getComputerChoice()));
-  });
-});
+// options that will be chosen by the computer
+const options = ['Rock', 'Paper', 'Scissors'];
 
-const options = ['rock', 'paper', 'scissors'];
+// scores of each player
+let playerScore = 0;
+let computerScore = 0;
 
+// creating reset button
+let resetButton;
+
+// gives computer a choice
 function getComputerChoice() {
   let chosenOption = Math.floor(Math.random() * options.length);
   return options[chosenOption];  
 }
 
+// plays a round, updates scores
 function playRound(playerSelection, computerSelection) {
-  if(playerSelection == 'rock') {
-    if(computerSelection == 'rock') {
-      return 'tie';
+  if(playerSelection == 'Rock') {
+    if(computerSelection == 'Rock') {
+      return 'Both sides sent ' + playerSelection + '.';
     }
-    else if(computerSelection == 'paper') {
-      return 'loss';
+    else if(computerSelection == 'Paper') {
+      computerScore++;
+      return 'You lose the round! ' + playerSelection + ' loses to ' + computerSelection + '!';
     }
-    else if(computerSelection == 'scissors') {
-      return 'win';
+    else if(computerSelection == 'Scissors') {
+      playerScore++;
+      return 'You win the round! ' + playerSelection + ' beats ' + computerSelection + '!';
     }
   }
-  else if(playerSelection == 'paper') {
-    if(computerSelection == 'paper') {
-      return 'tie';
+  else if(playerSelection == 'Paper') {
+    if(computerSelection == 'Paper') {
+      return 'Both sides sent ' + playerSelection + '.';
     }
-    else if(computerSelection == 'scissors') {
-      return 'loss';
+    else if(computerSelection == 'Scissors') {
+      computerScore++;
+      return 'You lose the round! ' + playerSelection + ' loses to ' + computerSelection + '!';
     }
     else if(computerSelection == 'Rock') {
-      return 'win';
+      playerScore++;
+      return 'You win the round! ' + playerSelection + ' beats ' + computerSelection + '!';
     }        
   }
-  else if(playerSelection == 'scissors') {
-    if(computerSelection == 'scissors') {
-      return 'tie';
+  else if(playerSelection == 'Scissors') {
+    if(computerSelection == 'Scissors') {
+      return 'Both sides sent ' + playerSelection + '.';
     }
-    else if(computerSelection == 'rock') {
-      return 'loss';
-    }
-    else if(computerSelection == 'paper') {
-      return 'win';
-    }
-  }
-  else {
-    return 'nil';
-  }
-}
-
-function game() {
-  
-  let playerScore = 0;
-  let computerScore = 0;
-  
-  /* 
-  while(playerScore < 3 && computerScore < 3) {
-
-    let player = prompt("Choose rock, paper, or scissors");
-    let computer = getComputerChoice();
-    let result = playRound(player, computer);
-
-    player = player.charAt(0).toUpperCase() + player.substring(1).toLowerCase();
-    
-    if(result == 'win') {
-      console.log('You win the round! ' + player + ' beats ' + computer + '!');
-      playerScore++;
-      console.log('Player: ' + playerScore);
-      console.log('Computer: ' + computerScore);
-    }
-    else if(result == 'loss') {
+    else if(computerSelection == 'Rock') {
       computerScore++;
-      console.log('You lose the round! ' + player + ' loses to ' + computer + '!');
-      console.log('Player: ' + playerScore);
-      console.log('Computer: ' + computerScore);
+      return 'You lose the round! ' + playerSelection + ' loses to ' + computerSelection + '!';
     }
-    else if(result == 'tie') {
-      console.log('Both sides sent ' + player);
-      console.log('Player: ' + playerScore);
-      console.log('Computer: ' + computerScore);
-    }
-    else if(result == 'nil') {
-      console.log('Please input rock, paper, or scissors');
+    else if(computerSelection == 'Paper') {
+      playerScore++;
+      return 'You win the round! ' + playerSelection + ' beats ' + computerSelection + '!';
     }
   }
+}
 
-  if(playerScore > computerScore) {
-    console.log('You win!');
+// Identifies the buttons, and adds an event that causes playRound to occur with 
+// a random computer choice
+const buttons = document.querySelectorAll('.options');
+
+buttons.forEach(button => {
+  button.addEventListener('click', function(e) {
+    const playerOption = this.id;
+    const computerOption = getComputerChoice();
+    let num = 0;
+
+    result.textContent = playRound(playerOption, computerOption);
+    one.textContent = 'Player: ' + playerOption;
+    two.textContent = 'Computer: ' + computerOption;
+    score.textContent = playerScore + ' - ' + computerScore;
+    checkScores();
+  });
+});
+
+function checkScores() {
+  if(playerScore > 2 || computerScore > 2) {
+    endGame();
+  }
+}
+
+
+function endGame() {
+  if(playerScore == 3) {
+    result.textContent = 'Congratulations! You won!';
   }
   else {
-    console.log('You lose!');
+    result.textContent = 'Sorry, the computer seems to have bested you.';
   }
-  */
+
+  document.querySelectorAll(".options").forEach(e => e.disabled = true);
+
+  question.textContent = 'Would you like to play again?';
+
+  resetButton = document.createElement('button');
+  resetButton.textContent = 'YES';
+  endgame.appendChild(resetButton);
+
+  resetButton.addEventListener('click', resetGame);
 }
-game();
+
+function resetGame() {
+  
+  let resetAll = document.querySelectorAll('.reset');
+  for(let element of resetAll) {
+    element.textContent = '';
+  }
+
+  endgame.removeChild(resetButton);
+
+  playerScore = 0;
+  computerScore = 0;
+  document.querySelectorAll(".options").forEach(e => e.disabled = false);
+}
